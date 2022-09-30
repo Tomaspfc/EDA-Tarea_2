79afd991461a29ad8e43f9d7d57a647abff8a4c9
Tomas Pfingsthorn <tpfingsthorn@miuandes.cl>
2022-09-30 15:46:46 -0300

    done

--- main.cpp
+++ main.cpp
@@ -1,23 +1,84 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "stack.hpp"
+#include "include/stack.hpp"
+#include "include/node.hpp"
 
-int checkFile(std::string *filename) {
+void checkFile(std::string *filename, std::string* logfile)
+{
     std::ifstream file;
     file.open(*filename);
-    char ch;
-    while (file.good()) {
-        file.get(ch);
-        std::cout << ch << "-";
-    }
-     
+    
+    std::ofstream log;
+    log.open(*logfile);
 
-    return 0;
+    char ch;
+    int in_tag, closing = 0, errors = 0, line = 1;
+
+    std::string tag_name;
+    eda::Stack stack;
+    std::string last_tag;
+
+    while (file.good())
+    {
+        file.get(ch);
+
+        switch (ch)
+        {
+        case '<':
+            in_tag = 1;
+            tag_name = "";
+            break;
+
+        case '>':
+            // std::cout << "tag name: " << tag_name << std::endl;
+            if (closing) {
+                last_tag = stack.top()->getData();
+                if (last_tag != tag_name) {
+                    log << "Error in line " << line << ": expecting </" << last_tag << ">, got </" << tag_name << ">\n";
+                    errors ++;
+                } else {
+                    log << "tag <" << tag_name << "> ok" << std::endl;
+                    stack.pop();
+                }
+            } else {
+                stack.push(tag_name);
+            }
+            in_tag = 0;
+            closing = 0;
+            break;
+
+        case '/':
+            closing = 1;
+            break;
+
+        case '\n':
+            line ++;
+            break;
+
+        default:
+            if (in_tag)
+            tag_name += ch;
+            break;
+        }
+    }
+    if (!errors) {
+        log << "0 errors" << std::endl;
+    }
 }
 
-int main(int argc, char** argv) {
+std::string getName(char* filename) {
+    std::string name = "";
+    for (int i = 0; filename[i] != '.'; i++)
+        name += filename[i];
+    return name;
+}
+
+int main(int argc, char **argv)
+{
     std::string filename = argv[argc - 1];
-    checkFile(&filename);
+    std::string name = getName(argv[argc - 1]) + ".log";
+    checkFile(&filename, &name);
+    std::cout << "output -> " << name << std::endl;
     return 0;
 }
